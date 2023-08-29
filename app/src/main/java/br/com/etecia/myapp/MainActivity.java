7767d0a3ff27ba3cf20b553e04f665bf929aeead
CynGL <cyntialino09@gmail.com>
2023-08-29 20:07:35 -0300

    commit

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -26,5 +26,30 @@ public class MainActivity extends AppCompatActivity {
         myViewPagerAdapter = new MyViewPagerAdapter(this);
 
         viewPager2.setAdapter(myViewPagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                tabLayout.getTabAt(position).select();
+            }
+        });
     }
 }
